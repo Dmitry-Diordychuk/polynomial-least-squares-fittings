ab8f3878c9172677f35ec28e2618c8242d7590bd
Diord <Diord@DESKTOP-6B09NPQ>
2019-11-16 00:20:43 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsLab2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_plot = new System.Windows.Forms.Button();
+            this.textBoxDegree = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_save = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(547, 543);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_plot
+            // 
+            this.button_plot.Location = new System.Drawing.Point(494, 582);
+            this.button_plot.Name = "button_plot";
+            this.button_plot.Size = new System.Drawing.Size(75, 36);
+            this.button_plot.TabIndex = 1;
+            this.button_plot.Text = "Plot";
+            this.button_plot.UseVisualStyleBackColor = true;
+            this.button_plot.Click += new System.EventHandler(this.button_plot_Click);
+            // 
+            // textBoxDegree
+            // 
+            this.textBoxDegree.Location = new System.Drawing.Point(115, 592);
+            this.textBoxDegree.Name = "textBoxDegree";
+            this.textBoxDegree.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDegree.TabIndex = 2;
+            this.textBoxDegree.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 595);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Poly degree";
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(269, 582);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 36);
+            this.button_save.TabIndex = 4;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(575, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(738, 544);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 652);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDegree);
+            this.Controls.Add(this.button_plot);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_plot;
+        private System.Windows.Forms.TextBox textBoxDegree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsLab2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PLplot;
+
+namespace WindowsFormsLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTable();
+            pictureBox1.ImageLocation = "white.png";
+        }
+        private DataTable _dt;
+        private void LoadTable()
+        {
+            var dt = new DataTable();
+            using (var sr = new StreamReader("data.csv", Encoding.Default))
+            {
+                var headers = sr.ReadLine().Split(';');
+                foreach (var header in headers) dt.Columns.Add(header);
+
+                while (!sr.EndOfStream)
+                {
+                    var rows = sr.ReadLine().Split(';');
+                    var dr = dt.NewRow();
+                    for (var i = 0; i < headers.Length; i++) dr[i] = rows[i];
+
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            dataGridView1.DataSource = dt;
+            _dt = dt;
+        }
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            using (var sw = new StreamWriter("data.csv"))
+            {
+                sw.WriteLine("X;Y;P");
+                foreach (DataRow row in _dt.Rows) sw.WriteLine("{0};{1};{2}", row[0], row[1], row[2]);
+            }
+        }
+
+        private double SCR = 0.0;
+        private void button_plot_Click(object sender, EventArgs e)
+        {
+            InitInput(out List<Double> X, out List<double> Y, out List<double> P, out int degree);
+
+            double[,] A = new double[degree,degree];
+
+            for(int i = 0; i < degree; i++)
+                for (int j = 0; j < degree; j++)
+                {
+                    A[i,j] = PolySumA(i, j, X, P);
+                }
+
+            double[] B = new double[degree];
+
+            for (int i = 0; i < degree; i++)
+            {
+                B[i] = PolySumB(i, X, Y, P);
+            }
+
+            double[] coef = new double[degree];
+            coef = GaussMTB(A, B, degree);
+
+            List<double> resultX = new List<double>();
+            List<double> resultY = new List<double>();
+            for (double x0 = X.First()-SCR; x0 < X.Last()+SCR; x0 = x0 + 0.1)
+            {
+                resultX.Add(x0);
+                resultY.Add(y(coef, degree, x0));
+            }
+            
+            using (PLStream pl = new PLStream())
+            {
+                pl.sdev("pngcairo");                // png rendering
+                pl.sfnam("data.png");                 // output filename
+                pl.spal0("cmap0_alternate.pal");   // alternate color palette
+                //pl.col0(1);
+                //pl.col0(100);
+
+                pl.init();
+                pl.env(X.Min()-SCR, X.Max()+SCR, Y.Min()-SCR, Y.Max()+SCR, AxesScale.Independent, AxisBox.BoxTicksLabelsAxes);
+                pl.lab("x", "Plot", "Plot"); //y=3 x#u2#d
+
+                pl.poin(X.ToArray(),Y.ToArray(), 'o');
+                pl.line(resultX.ToArray(), resultY.ToArray());
+
+                pl.eop();
+            }
+            pictureBox1.ImageLocation = "data.png";
+
+        }
+
+        private void InitInput(out List<double> X, out List<double> Y, out List<double> P, out int degree)
+        {
+            X = new List<double>();
+            Y = new List<double>();
+            P = new List<double>();
+
+            degree = Int32.Parse(textBoxDegree.Text);
+
+            foreach (DataRow row in _dt.Rows)
+            {
+                X.Add(Double.Parse(row[0].ToString()));
+                Y.Add(Double.Parse(row[1].ToString()));
+                P.Add(Double.Parse(row[2].ToString()));
+            }
+        }
+
+        static double PolySumA(int i, int j, List<double> X, List<double> P)
+        {
+            double sum = 0;
+            for (int k = 0; k < X.Count; k++)
+            {
+                sum += P[k] * Math.Pow(X[k], i + j);
+            }
+            return sum;
+        }
+
+        static double PolySumB(int i, List<double> X, List<double> Y, List<double> P)
+        {
+            double sum = 0;
+            for (int j = 0; j < X.Count; j++)
+            {
+                sum += P[j] * Y[j] * Math.Pow(X[j], i);
+            }
+            return sum;
+        }
+
+        static double[] GaussMTB(double[,] z, double[] y, int n)
+        {
+            double[,] a = new double[n, n];
+            Array.Copy(z, a, z.Length);
+            double[] b = new double[n];
+            Array.Copy(y, b, y.Length);
+
+            //PrintM("GaussM", a, b, n);
+
+            //Часть 1
+            //Модифицированная верхнее приобразование (получение нижних нулей.
+            for (int k = 0; k < n; k++)
+            {
+                double d = a[k, k];
+                for (int j = k; j < n; j++)
+                    a[k, j] /= d;
+                b[k] /= d;
+                for (int i = k + 1; i < n; i++)
+                {
+                    double r = a[i, k];
+                    for (int j = k; j < n; j++)
+                        a[i, j] -= a[k, j] * r;
+                    b[i] -= b[k] * r;
+                }
+            }
+
+
+            //Часть 2
+            for (int k = n - 1; k > 0; k--)
+            {
+                for (int i = k - 1; i >= 0; i--)
+                {
+                    b[i] -= b[k] * a[i, k];
+                    a[i, k] = 0.0;
+                }
+            }
+
+            return b;
+            //PrintM("GaussMTB", a, b, n);
+        }
+
+        static double y (double[] coefficients, int degree, double x)
+        {
+            double result = 0;
+            for (int i = 0; i < degree; i++)
+            {
+                result += coefficients[i] * Math.Pow(x,i);
+            }
+
+            return result;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+                SCR += 10;
+            if (e.Button == MouseButtons.Right)
+                SCR -= 10;
+            button_plot_Click(this,e);
+        }
+    }
+}
